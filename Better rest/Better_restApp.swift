66a3d8647a5596210a07612391ab193931f774a8
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-09-30 17:12:16 +0200

    Initial Commit

---
+++ Better rest/Better_restApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_restApp.swift
+//  Better rest
+//
+//  Created by Natalia Nikiforuk on 30/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Better_restApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
